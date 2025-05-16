6c1673a8cfe55ffc59a4231b5935bfbfcb622520
naseerAhmadKhanDK <naseeruomian50@gmail.com>
2025-05-16 11:33:02 +0200

    comment added

--- TestString/Program.cs
+++ TestString/Program.cs
@@ -1,4 +1,6 @@
-﻿string str = "Denmark";
+﻿/* Á simple program that print the given string in upword and downword direction
+ * adding one char at the end and at the end of the string print the whole string and voice versa*/
+string str = "Denmark";
 string upwordstring = "";
 
 
