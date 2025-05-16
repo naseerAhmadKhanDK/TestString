3aa928dba3b5076a0d57f0395e5243b6809aa320
naseerAhmadKhanDK <naseeruomian50@gmail.com>
2025-05-16 11:37:05 +0200

    organize

--- TestString/Program.cs
+++ TestString/Program.cs
@@ -9,14 +9,11 @@ string upwordstring = "";
 for(int i = 0; i < str.Length; i++)
 {
     upwordstring += str[i];
-    Console.WriteLine(upwordstring);
-   
+    Console.WriteLine(upwordstring);  
     
 }
 for(int j=str.Length-1; j>=0; j--)
 {
    string downstring1 = str.Substring(0,j);
-
-  Console.WriteLine(downstring1);
-    
+  Console.WriteLine(downstring1);    
 }
