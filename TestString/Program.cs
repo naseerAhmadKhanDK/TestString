d864cae9995f028409043d6fe62a3820a7c7389b
naseerAhmadKhanDK <naseeruomian50@gmail.com>
2025-05-16 11:28:33 +0200

    Add project files.

---
+++ TestString/Program.cs
@@ -0,0 +1,20 @@
+﻿string str = "Denmark";
+string upwordstring = "";
+
+
+
+
+for(int i = 0; i < str.Length; i++)
+{
+    upwordstring += str[i];
+    Console.WriteLine(upwordstring);
+   
+    
+}
+for(int j=str.Length-1; j>=0; j--)
+{
+   string downstring1 = str.Substring(0,j);
+
+  Console.WriteLine(downstring1);
+    
+}
